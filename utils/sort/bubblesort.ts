6e60aed8bce57b0ddadddd9797d1574b3e95ae2b
Marcelo Rissette <marcelo.rissette@vidyatec.com>
2023-07-15 18:39:47 -0300

    Refac: bubblesort

--- utils/sort/bubblesort.ts
+++ utils/sort/bubblesort.ts
@@ -4,19 +4,23 @@ export default function* bubbleSort(arr: number[]): Generator<{
   idx2?: number | undefined;
   pivot: number;
 }> {
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr.length - i - 1; j++) {
+  const arrSize = arr.length;
+
+  for (let i = 0; i < arrSize; i++) {
+    for (let j = 0; j < arrSize - i - 1; j++) {
+      const current = arr[j];
+      const next = arr[j + 1];
+
       yield { arr: arr, idx: j, idx2: j + 1, pivot: j + 1 };
 
-      if (arr[j] > arr[j + 1]) {
-        const tmp = arr[j];
-
-        arr[j] = arr[j + 1];
-        arr[j + 1] = tmp;
+      if (current > next) {
+        arr[j] = next;
+        arr[j + 1] = current;
 
         yield { arr: arr, idx: j, idx2: j + 1, pivot: j + 1 };
       }
     }
   }
+
   return { arr: arr, idx: -1, idx2: -1, pivot: -1 };
 }
